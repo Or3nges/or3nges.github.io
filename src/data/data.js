c5573a38a25bf44cd615f6822334b243ab4c92fb
Lohani Pratik <pratik.lohani@student.howest.be>
2025-12-02 19:11:37 +0100

    Add project in data.js

--- src/data/data.js
+++ src/data/data.js
@@ -9,17 +9,17 @@ export const navLinks = [
 export const projects = [
   {
     id: 1,
-    title: "HowestPrime - Cinema App",
+    title: "HowestPrime - Cinema Project",
     description: "A microservices-based cinema management system using Domain Driven Design. Features a web platform for reservations (.NET Blazor) and an Android app for staff (Kotlin). Infrastructure managed with Terraform on Azure.",
-    tech: [".NET", "Kotlin", "Azure", "Terraform", "Microservices"],
+    tech: [".NET", "TypeScript", "Kotlin", "Azure", "Terraform", "Microservices"],
     github: "https://github.com/Or3nges?tab=repositories&q=howestprime",
     live: "#"
   },
   {
     id: 2,
     title: "Splendor - Digital Card Game",
-    description: "An online multiplayer version of the board game Splendor. Built with a Vue.js frontend and a robust Java backend handling game logic, turn management, and scoring. Includes comprehensive JUnit tests.",
-    tech: ["Vue.js", "Java", "JUnit", "Rest API"],
+    description: "An online multiplayer version of the board game Splendor. Built with a vanilla javascript frontend and a robust Java backend handling game logic, turn management, and scoring. Includes comprehensive JUnit tests.",
+    tech: ["Javascript", "Java", "JUnit", "Rest API"],
     github: "https://github.com/Or3nges?tab=repositories&q=Splendor",
     live: "#"
   },
@@ -37,6 +37,14 @@ export const projects = [
     description: "Intensive project on AI/ML for autonomous systems. Improved a Python AI model to enable a small car to navigate autonomously using computer vision and real-time model training.",
     tech: ["Python", "AI/ML", "Computer Vision"],
     github: "https://github.com/Or3nges/Autonomous-AWS-Car",
+    live: "#"
+  },
+  {
+    id: 5,
+    title: "Labyrinth - Digital Board Game",
+    description: "A digital adaptation of the classic Labyrinth board game. Developed a Java backend to manage game logic and a JavaScript frontend for user interaction. Implemented multiplayer functionality and game state persistence.",
+    tech: ["Javascript", "Java", "JUnit", "Rest API"],
+    github: "https://github.com/Or3nges?tab=repositories&q=Labyrinth",
     live: "#"
   },
 ];
