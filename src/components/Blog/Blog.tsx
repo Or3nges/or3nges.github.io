108d45e17c496e7f84c4cbba5b93179a6c6474cb
Lohani Pratik <pratik.lohani@student.howest.be>
2026-05-16 14:38:31 +0200

    Fix contact component overlapping with blog

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -92,7 +92,7 @@ const Blog = () => {
         </motion.div>
 
         {/* Blog Grid */}
-        <div className="h-[700px]">
+        <div className="w-full">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
               {filteredBlogs.map((blog, index) => (
                 <motion.div
